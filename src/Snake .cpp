e243c96d9136f97136822ef8e59294fb15991ceb
Ashutosh <am3718440@gmail.com>
2024-02-13 14:05:58 +0530

    Added Snake functioning parameters'
    Signed-off-by: Ashutosh <am3718440@gmail.com>

--- src/Snake .cpp
+++ src/Snake .cpp
@@ -1,6 +1,23 @@
 #include "Snake .h"
 
-Snake ::Snake ()
+Snake ::Snake (COORD pos, int speed)
 {
-    //ctor
+    this->pos=pos;
+    this->speed=speed;
+    len=1;
+    direction='n';
+}
+void Snake::changeDirection(char dir){
+    direction=dir;
+}
+
+void Snake::moveSnake()
+{
+    switch(direction)
+    {
+        case 'u': pos.Y = pos.Y- speed; break;
+        case 'u': pos.Y = pos.Y- speed; break;
+        case 'u': pos.Y = pos.Y- speed; break;
+
+    }
 }

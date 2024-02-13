264969fd44463efb9e029a896a1f32077d50b975
Ashutosh <am3718440@gmail.com>
2024-02-13 13:08:36 +0530

    Added board code on terminal
    Signed-off-by: Ashutosh <am3718440@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
+#include <cstdlib>
 #define WIDTH 50
 #define HEIGHT 25
 using namespace std;
-
+int x=10,y=10;
 void board(){
     for(int i=0;i<HEIGHT;i++)
     {
-        cout<<"#";
+        cout<<"\t\t#";
         for(int j=0;j<WIDTH-2;j++)
         {
             if(i==0 || i==HEIGHT-1)
                 cout<<'#';
+            else if(i==y && j==x)
+                cout<<'0';
             else
                 cout<<' ';
         }
@@ -19,5 +22,9 @@ void board(){
 }
 int main()
 {
-    board();
+    while(true){
+        board();
+        x++;
+        system("cls");
+    }
 }
